88d26c0caf14d4e9e69c0494b743e3dca73e27c0
merca17 <juanmercado5331@gmail.com>
2022-12-15 14:12:13 -0300

    added sensors

--- ultrasonico/ultrasonic_to_LaserScan.py
+++ ultrasonico/ultrasonic_to_LaserScan.py
@@ -6,6 +6,7 @@ from sensor_msgs.msg import LaserScan
 from math import pi as mpi
 from rclpy import qos
 from gpiozero import DistanceSensor
+import math as m
 sensor = DistanceSensor(5,6, queue_len=10, max_distance= 5)
 sensor1 = DistanceSensor(23,24, queue_len=10, max_distance= 5)
 sensor2 = DistanceSensor(19,26, queue_len=10, max_distance= 5)
@@ -31,9 +32,9 @@ class UltrasonicMsgsNode(Node):
         msg.header.frame_id = 'laser_frame'
         msg.header.stamp = Node.get_clock(self).now().to_msg()
 
-        msg.angle_min = mpi/4 - mpi/2                  # start angle of the scan [rad]
-        msg.angle_max = (mpi/4)*3 - mpi/2          # end angle of the scan [rad]
-        msg.angle_increment = mpi/4    # angular distances between measurements [rad]
+        msg.angle_min = -mpi/2                 # start angle of the scan [rad]
+        msg.angle_max = mpi/2         # end angle of the scan [rad]
+        msg.angle_increment = mpi/200    # angular distances between measurements [rad]
 
         msg.time_increment = 1.0/3        # time between measurements [seconds] if your scarnner is moving, this will be used in interpolating position of 3d points
         msg.scan_time = 1.0                 # time between scans
@@ -41,9 +42,25 @@ class UltrasonicMsgsNode(Node):
         msg.range_min = 0.05                 # minimum range value [m]
         msg.range_max = 3.0                 # maximum range value [m]
 
-        msg._ranges = [sensor.distance, sensor1.distance, sensor2.distance]                    # range data [m] (Note: values < range_min or > range_max should be descarted)
+        for j in range(0,201):
+            i=j*msg.angle_increment
+            d1 = sensor.distance
+            d2 = sensor1.distance
+            d3 = sensor2.distance
+            if(i<=mpi/3):
+                msg._ranges.append(d1)
+            elif(i>mpi/3 and i<=2*mpi/3):
+                msg._ranges.append(d2)
+            else:
+                msg._ranges.append(d3)
+            if(d1<0.1 or d2<0.1 or d3<0.1):
+                msg.intensities.append(1000)
+            else:
+                msg.intensities.append(10)            
+            
+        # msg._ranges = [sensor.distance, sensor1.distance, sensor2.distance]                    # range data [m] (Note: values < range_min or > range_max should be descarted)
     
-        msg.intensities = [2.3, 20.5, 4.2, 215.1,]                # intensity data (if not provided leave the array empty)
+        #msg.intensities = []                # intensity data (if not provided leave the array empty)
 
         #publishing
         self.laser_scan_pub_.publish(msg)
